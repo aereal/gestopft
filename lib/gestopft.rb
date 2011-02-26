f859e3204ebf4e9367689de0402047de39b3e41d
aereal <aereal.h@gmail.com>
2011-02-26 22:31:24 +0900

    add Gestopft.required, #run

--- lib/gestopft.rb
+++ lib/gestopft.rb
@@ -2,5 +2,21 @@
 
 module Gestopft
 	VERSION = '0.0.1'
+
+	class Error < ::StandardError; end
+	class NotSatisfiedRequirements < Error; end
 end
 
+class Gestopft::App
+	def self.required(*args)
+		(@@requirements ||= []).concat(args.map {|arg|
+			"--" + arg.to_s
+		})
+	end
+
+	def run(argv)
+		unless @@requirements.all? {|req| argv.include? req }
+			raise Gestopft::NotSatisfiedRequirements
+		end
+	end
+end
--- spec/gestopft_spec.rb
+++ spec/gestopft_spec.rb
@@ -1,4 +1,30 @@
 #!spec
 # -*- coding: utf-8 -*-
 # vim: set ft=ruby:
- 
+
+require "gestopft"
+
+describe Gestopft::App do
+	subject do
+		Class.new(Gestopft::App)
+	end
+
+	describe ".required" do
+		before :all do
+			subject.module_eval do
+				required :required_option
+			end
+		end
+
+		it "succeeds when given complete arguments." do
+			expect { subject.new.run(['--required_option']) }.
+				should_not raise_error(Gestopft::NotSatisfiedRequirements)
+		end
+
+		it "fails when given incomplete arguments." do
+			expect { subject.new.run([]) }.
+				should raise_error(Gestopft::NotSatisfiedRequirements)
+		end
+	end
+end
+
