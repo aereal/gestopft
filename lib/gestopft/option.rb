b3e33588332d96ae8657c6db9d4e12c144be38bd
aereal <aereal.h@gmail.com>
2011-03-18 14:11:41 +0900

    impl: Gestopft::Option#require_args? #arity

--- lib/gestopft/option.rb
+++ lib/gestopft/option.rb
@@ -1,14 +1,23 @@
 # encoding: utf-8
 
 class Gestopft::Option
-	attr_reader :name
+	attr_reader :name, :params
 
-	def initialize(name, expect={})
+	def initialize(name, args={})
 		@name = name
+		@params = args[:params]
 	end
 
 	def option_name
 		@name.to_option
+	end
+
+	def require_args?
+		!!@params
+	end
+
+	def arity
+		@params.nil? ? -1 : @params.size
 	end
 end
 
--- spec/gestopft/option_spec.rb
+++ spec/gestopft/option_spec.rb
@@ -10,9 +10,35 @@ describe Gestopft::Option do
 		Gestopft::Option.new(:my_option)
 	end
 
-	describe "#option_name" do
-		it "is option string." do
-			subject.option_name.should == '--my-option'
+	it "#option_name is option string." do
+		subject.option_name.should == '--my-option'
+	end
+
+	context "which requires parameters." do
+		subject do
+			Gestopft::Option.new(:delay, :params => %w(minute))
+		end
+
+		it "#require_args? is true" do
+			subject.require_args?.should be_true
+		end
+
+		it "#arity is the arity of option." do
+			subject.arity.should == 1
+		end
+	end
+
+	context "which requires no parameter." do
+		subject do
+			Gestopft::Option.new(:verbose)
+		end
+
+		it "#require_args? is false" do
+			subject.require_args?.should be_false
+		end
+
+		it "#arity is magic number which means the option takes no params." do
+			subject.arity.should == -1
 		end
 	end
 end
