f841f356ec451b096247e9514b2396dab03f091d
aereal <aereal.h@gmail.com>
2011-03-18 17:01:10 +0900

    refactoring: use Gestopft::Option

--- lib/gestopft.rb
+++ lib/gestopft.rb
@@ -16,24 +16,30 @@ end
 class Gestopft::App
 	include Gestopft::Constants
 
-	@expected_options = {}
-
 	def self.expected_options
-		@expected_options ||= {}
+		@expected_options ||= []
 	end
 
-	def self.options(*opts)
-		with_args, *no_args =
-			opts.last.class == Hash ? opts.reverse : [{}, *opts]
-		expected_options.update(with_args).update(
-			Hash[no_args.zip(
-				Array.new(no_args.size, true)
-			)]
-		)
+	#def self.options(*opts)
+	#	with_args, *no_args =
+	#		opts.last.class == Hash ? opts.reverse : [{}, *opts]
+	#	expected_options.update(with_args).update(
+	#		Hash[no_args.zip(
+	#			Array.new(no_args.size, true)
+	#		)]
+	#	)
+	#end
+
+	def self.option(name, args={})
+		expected_options << Gestopft::Option.new(name, args)
 	end
 
 	def self.run(argv)
 		new(argv).parse_arg!
+	end
+
+	class << self
+		alias_method :opt, :option
 	end
 
 	attr_reader :given_options
@@ -45,38 +51,21 @@ class Gestopft::App
 	end
 
 	def parse_arg!
-		@expected_options.select {|name, expect| expect }.each do |name, expect|
-			opt = name.to_option
-			case expect
-			when true
-				if @argv.delete(opt)
-					@given_options[name] = expect
-				else
-					raise NotSatisfiedRequirements
-				end
-			when Array # Optional argument
-				if pos = @argv.find_index(opt)
-					arg = @argv[pos + 1]
-					if arg && arg.option?
-						@argv.delete(opt)
-						@given_options[name] = true
-					else
-						opt, param = @argv.slice!(pos, 2)
-						@given_options[name] = param
-					end
-				end
-			when Module
-				if pos = @argv.find_index(opt)
-					arg = @argv[pos + 1]
-					if arg && !arg.option?
-						opt, param = @argv.slice!(pos, 2)
-						@given_options[name] = param
-					else
+		@expected_options.each do |opt|
+			if opt.require_args?
+				if pos = @argv.find_index(opt.option_name)
+					params = @argv[pos + 1, opt.arity]
+					unless (
+						params &&
+						params.none? {|prm| prm.option? } &&
+						params.size == opt.arity
+					)
 						raise NotSatisfiedRequirements
 					end
-				else
-					raise NotSatisfiedRequirements
+					@given_options[opt.name] = params
 				end
+			else
+				@given_options[opt.name] = @argv.delete(opt.option_name)
 			end
 		end
 	else
--- spec/gestopft_spec.rb
+++ spec/gestopft_spec.rb
@@ -6,73 +6,53 @@ require "gestopft"
 include Gestopft::Constants
 
 describe Gestopft::App do
-	describe ".options" do
-		subject do
-			Class.new(Gestopft::App)
-		end
+	subject do
+		Class.new(Gestopft::App)
+	end
 
-		context "given a option which requires no arguments." do
-			before :all do
-				subject.module_eval do
-					options :with_no_args
-				end
-			end
-
-			it "has a option which given." do
-				subject.run(["--with-no-args"]).given_options.
-					should include(:with_no_args)
-			end
-
-			it "does not have a option which does not given." do
-				subject.run(["--with-no-args"]).given_options.
-					should_not include(:does_not_given)
-			end
-
-			it "succeeds when given a option which requires no arguments." do
-				expect { subject.run(["--with-no-args"]) }.
-					should_not raise_error(Error)
-			end
-
-			it "succeeds when not given a option which is required." do
-				expect { subject.run([]) }.
-					should raise_error(NotSatisfiedRequirements)
+	context "given a option which requires no arguments." do
+		before :all do
+			subject.module_eval do
+				option :with_no_args
 			end
 		end
 
-		context "a option which may take a argument." do
-			before :all do
-				subject.module_eval do
-					options :with_args => [String]
-				end
-			end
+		it "has a option which given." do
+			subject.run(%w(--with-no-args)).given_options.
+				should include(:with_no_args)
+		end
 
-			it "has a option which given with taken value." do
-				subject.run(%w(--with-args myargs)).given_options.
-					should include(:with_args => 'myargs')
-			end
+		it "does not have a option which does not given." do
+			subject.run(%w(--with-no-args)).given_options.
+				should_not include(:does_not_given)
+		end
 
-			it "has a option which given without any arguments." do
-				subject.run(%w(--with-args)).given_options.
-					should include(:with_args)
+		it "succeeds when given a option which requires no arguments." do
+			expect { subject.run(%w(--with-no-args)) }.
+				should_not raise_error(Error)
+		end
+
+		it "succeeds when not given a option which is required." do
+			expect { subject.run([]) }.
+				should_not raise_error(NotSatisfiedRequirements)
+		end
+	end
+
+	context "a option which require a argument." do
+		before :all do
+			subject.module_eval do
+				option :delay, :params => [:minute]
 			end
 		end
 
-		context "a option which require a argument." do
-			before :all do
-				subject.module_eval do
-					options :require_args => String
-				end
-			end
+		it "has a option which given with taken parameter." do
+			subject.run(%w(--delay 5)).given_options.
+				should include(:delay => ['5'])
+		end
 
-			it "has a option which given with taken parameter." do
-				subject.run(%w(--require-args foo)).given_options.
-					should include(:require_args => 'foo')
-			end
-
-			it "does not have a option which given without any arguments." do
-				expect { subject.run(%w(--require-args)) }.
-					should raise_error(NotSatisfiedRequirements)
-			end
+		it "does not have a option which given without any arguments." do
+			expect { subject.run(%w(--delay)) }.
+				should raise_error(NotSatisfiedRequirements)
 		end
 	end
 end
